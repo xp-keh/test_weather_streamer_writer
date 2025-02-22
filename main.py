1098fb1bfb277ec640d44597b47a34bb597ab97e
ernestbenedictus <m010d4ky1623@bangkit.academy>
2025-02-22 22:05:55 +0700

    refactored kafka consumer to use threading and simultaneously stream via sse

--- main.py
+++ main.py
@@ -1,25 +1,67 @@
+import threading
 import asyncio
+from dotenv import load_dotenv 
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from aiokafka import AIOKafkaConsumer
+from config.utils import get_env_value
+
+load_dotenv()
+
+kafka_broker = get_env_value('KAFKA_BROKER')
+kafka_consume_topic = get_env_value('KAFKA_CONSUME_TOPIC')
+kafka_consumer_group = get_env_value('KAFKA_CONSUMER_GROUP')
 
 app = FastAPI()
 
-KAFKA_BROKER = "weather_kafka:9097"
-TOPIC = "weather_raw"
-
-async def kafka_consumer():
-    """Async generator that consumes messages from Kafka."""
-    consumer = AIOKafkaConsumer(TOPIC, bootstrap_servers=KAFKA_BROKER, group_id="sse-group")
-    await consumer.start()
+class KafkaSSEConsumer:
+    """
+    Kafka Consumer that continuously listens to a topic and streams data via SSE.
+    """
+    def __init__(self, kafka_broker, topic, group_id):
+        self.kafka_broker = kafka_broker
+        self.topic = topic
+        self.group_id = group_id
+        self.loop = asyncio.new_event_loop()
+        self.consumer = AIOKafkaConsumer(
+            self.topic, 
+            bootstrap_servers=self.kafka_broker, 
+            group_id=self.group_id,
+            auto_offset_reset="earliest"
+        )
     
-    try:
-        async for message in consumer:
-            yield f"data: {message.value.decode('utf-8')}\n\n"  # SSE format
-    finally:
-        await consumer.stop()
+    async def start(self):
+        """Start the Kafka consumer."""
+        await self.consumer.start()
+
+    async def stop(self):
+        """Stop the Kafka consumer."""
+        await self.consumer.stop()
+    
+    async def consume(self):
+        """Async generator to consume messages."""
+        try:
+            async for message in self.consumer:
+                yield f"data: {message.value.decode('utf-8')}\n\n"
+        except Exception as e:
+            print(f"Error in consumer: {e}")
+
+    def run_consumer(self):
+        """Run the consumer in an event loop."""
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_until_complete(self.start())
+        self.loop.run_forever()
+
+sse_consumer = KafkaSSEConsumer(kafka_broker, kafka_consume_topic, kafka_consumer_group)
+
+t_consumer = threading.Thread(target=sse_consumer.run_consumer, daemon=True)
+t_consumer.start()
+
+@app.get("/ping")
+async def healthcheck():
+    return {"status": "healthy"}
 
 @app.get("/stream")
 async def stream():
     """SSE endpoint to send Kafka messages to the frontend."""
-    return StreamingResponse(kafka_consumer(), media_type="text/event-stream")
+    return StreamingResponse(sse_consumer.consume(), media_type="text/event-stream")
