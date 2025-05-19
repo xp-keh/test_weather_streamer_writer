bef00a3fad42f02a111043c21419ad707c9164dd
ernestbenedictus <m010d4ky1623@bangkit.academy>
2025-05-19 15:55:32 +0700

    refactor: changed data insertion mechanism

--- datastore/redis_store.py
+++ datastore/redis_store.py
@@ -2,7 +2,7 @@ import aioredis
 import logging
 import json
 from config.utils import get_env_value
-import logging
+from collections import defaultdict
 
 logger = logging.getLogger("redis_store")
 
@@ -45,3 +45,38 @@ async def clear_redis():
         logging.info("✅ Redis flush successful.")
     except Exception as e:
         logging.error(f"❌ Redis flush failed: {e}")
+
+
+async def get_weather_data_by_keys(keys: list[str]):
+    global redis_client
+    if redis_client is None:
+        await init_redis()
+
+    if not keys:
+        return []
+
+    try:
+        raw_values = await redis_client.mget(*keys)
+        return [json.loads(value) for value in raw_values if value]
+    except Exception as e:
+        logger.error(f"Failed to fetch weather data by keys: {e}")
+        return []
+
+async def group_keys_by_location() -> dict[str, list[str]]:
+    global redis_client
+    if redis_client is None:
+        await init_redis()
+
+    try:
+        keys = await redis_client.keys("weather:*")
+        grouped = defaultdict(list)
+
+        for key in keys:
+            if "_" in key:
+                _, location = key.rsplit("_", 1)
+                grouped[location].append(key)
+
+        return dict(grouped)
+    except Exception as e:
+        logger.error(f"Failed to group Redis keys by location: {e}")
+        return {}
--- writer/clickhouse_writer.py
+++ writer/clickhouse_writer.py
@@ -1,9 +1,11 @@
 import logging
 import clickhouse_connect
 import pytz
+import httpx
 from datetime import datetime, timezone, timedelta
 from config.utils import get_env_value
-from datastore.redis_store import get_all_weather_data, clear_redis
+# from datastore.redis_store import get_all_weather_data, clear_redis
+from datastore.redis_store import group_keys_by_location, get_weather_data_by_keys, clear_redis
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
@@ -13,6 +15,35 @@ CLICKHOUSE_USER = get_env_value("CLICKHOUSE_USER")
 CLICKHOUSE_PASSWORD = get_env_value("CLICKHOUSE_PASSWORD")
 
 gmt7 = pytz.timezone("Asia/Jakarta")
+
+locations = {
+    "TNTI": ("0.7718", "127.3667"),
+    "PMBI": ("-2.9024", "104.6993"),
+    "BKB": ("-1.1073", "116.9048"),
+    "SOEI": ("-9.7553", "124.2672"),
+    "MMRI": ("-8.6357", "122.2376"),
+}
+
+async def register_table_api(table_name, location, timestamp_str):
+    url = "http://85.209.163.202:4000/catalog/register"
+    data = {
+        "table_name": table_name,
+        "data_type": "weather",
+        "station_id": location,
+        "date": timestamp_str,
+        "lat": locations.get(location, ("unknown", "unknown"))[0],
+        "lon": locations.get(location, ("unknown", "unknown"))[1],
+    }
+    
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(url, data=data)
+            if response.status_code == 200:
+                logging.info(f"Registered table for {table_name} successfully")
+            else:
+                logging.error(f"Failed to register table for location {location}. Status: {response.status_code}")
+        except Exception as e:
+            logging.error(f"Exception registering table for location {location}: {e}")
 
 def format_unix_to_gmt7_string(unix_ts):
     try:
@@ -27,7 +58,7 @@ def safe_float(val: str) -> float:
         return float(val.replace(",", "."))
     except (ValueError, AttributeError):
         return 0.0 
-
+    
 async def bulk_write_to_clickhouse():
     try:
         clickhouse_client = clickhouse_connect.get_client(
@@ -41,68 +72,160 @@ async def bulk_write_to_clickhouse():
         logging.error(f"Failed to connect to ClickHouse: {e}")
         return
 
-    data = await get_all_weather_data()
-    
-    if not data:
+    grouped_keys = await group_keys_by_location()
+    if not grouped_keys:
         logging.info("No new data to write to ClickHouse.")
         return
 
     previous_hour = datetime.now() - timedelta(hours=1)
-    timestamp_str = previous_hour.strftime("%Y%m%d_%H")
-    # timestamp_str = datetime.now().strftime("%Y%m%d_%H")
-    table_name = f"weather_{timestamp_str}"
+    timestamp_str = previous_hour.strftime("%Y%m%d")
 
-    create_table_query = f"""
-    CREATE TABLE IF NOT EXISTS {table_name} (
-        location String,
-        lat Float64,
-        lon Float64,
-        temp Float32,
-        feels_like Float32,
-        temp_min Float32,
-        temp_max Float32,
-        pressure Int32,
-        humidity Int32,
-        wind_speed Float32,
-        wind_deg Int32,
-        wind_gust Float32,
-        clouds Int32,
-        timestamp Int32,
-        dt Int32,
-        dt_format String
-    ) ENGINE = MergeTree()
-    ORDER BY timestamp
-    """
-    clickhouse_client.command(create_table_query)
+    total_inserted = 0
 
-    data_to_insert = [
-        (
-            row["location"],
-            safe_float(row.get("lat", 0.0)),
-            safe_float(row.get("lon", 0.0)),
-            row.get("temp", 0.0), 
-            row.get("feels_like", 0.0),
-            row.get("temp_min", 0.0),
-            row.get("temp_max", 0.0),
-            row.get("pressure", 0), 
-            row.get("humidity", 0),
-            row.get("wind_speed", 0.0),
-            row.get("wind_deg", 0),
-            row.get("wind_gust", 0.0),
-            row.get("clouds", 0),
-            row.get("timestamp", 0),
-            row.get("dt", 0),
-            format_unix_to_gmt7_string(row.get("dt", 0)),
-        )
-        for row in data
-    ]
+    for location, keys in grouped_keys.items():
+        weather_data = await get_weather_data_by_keys(keys)
+        if not weather_data:
+            continue
 
-    utc_now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S GMT+0")
-    logging.info(f"Uploading {len(data)} records to ClickHouse at {utc_now}")
+        table_name = f"weather_{location}_{timestamp_str}"
 
-    clickhouse_client.insert(table_name, data_to_insert)
+        create_table_query = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            location String,
+            lat Float64,
+            lon Float64,
+            temp Float32,
+            feels_like Float32,
+            temp_min Float32,
+            temp_max Float32,
+            pressure Int32,
+            humidity Int32,
+            wind_speed Float32,
+            wind_deg Int32,
+            wind_gust Float32,
+            clouds Int32,
+            timestamp Int32,
+            dt Int32,
+            dt_format String
+        ) ENGINE = MergeTree()
+        ORDER BY timestamp
+        """
+        clickhouse_client.command(create_table_query)
 
-    await clear_redis()  
-    logging.info(f"Uploaded {len(data)} records to ClickHouse and cleared Redis.")
+        await register_table_api(table_name, location, timestamp_str)
+
+        data_to_insert = [
+            (
+                row["location"],
+                safe_float(row.get("lat", 0.0)),
+                safe_float(row.get("lon", 0.0)),
+                row.get("temp", 0.0), 
+                row.get("feels_like", 0.0),
+                row.get("temp_min", 0.0),
+                row.get("temp_max", 0.0),
+                row.get("pressure", 0), 
+                row.get("humidity", 0),
+                row.get("wind_speed", 0.0),
+                row.get("wind_deg", 0),
+                row.get("wind_gust", 0.0),
+                row.get("clouds", 0),
+                row.get("timestamp", 0),
+                row.get("dt", 0),
+                format_unix_to_gmt7_string(row.get("dt", 0)),
+            )
+            for row in weather_data
+        ]
+
+        clickhouse_client.insert(table_name, data_to_insert)
+        logging.info(f"Uploaded {len(data_to_insert)} records to table {table_name}.")
+
+        total_inserted += len(data_to_insert)
+
+    if total_inserted > 0:
+        await clear_redis()
+        logging.info(f"Uploaded a total of {total_inserted} records to ClickHouse and cleared Redis.")
+    else:
+        logging.info("No data was uploaded to ClickHouse.")
 
     clickhouse_client.close()
+
+
+
+# async def bulk_write_to_clickhouse():
+#     try:
+#         clickhouse_client = clickhouse_connect.get_client(
+#             host=CLICKHOUSE_HOST,
+#             port=8123,
+#             username=CLICKHOUSE_USER,
+#             password=CLICKHOUSE_PASSWORD,
+#             database=CLICKHOUSE_DATABASE
+#         )
+#     except Exception as e:
+#         logging.error(f"Failed to connect to ClickHouse: {e}")
+#         return
+
+#     data = await get_all_weather_data()
+    
+#     if not data:
+#         logging.info("No new data to write to ClickHouse.")
+#         return
+
+#     previous_hour = datetime.now() - timedelta(hours=1)
+#     timestamp_str = previous_hour.strftime("%Y%m%d_%H")
+#     # timestamp_str = datetime.now().strftime("%Y%m%d_%H")
+#     table_name = f"weather_{timestamp_str}"
+
+#     create_table_query = f"""
+#     CREATE TABLE IF NOT EXISTS {table_name} (
+#         location String,
+#         lat Float64,
+#         lon Float64,
+#         temp Float32,
+#         feels_like Float32,
+#         temp_min Float32,
+#         temp_max Float32,
+#         pressure Int32,
+#         humidity Int32,
+#         wind_speed Float32,
+#         wind_deg Int32,
+#         wind_gust Float32,
+#         clouds Int32,
+#         timestamp Int32,
+#         dt Int32,
+#         dt_format String
+#     ) ENGINE = MergeTree()
+#     ORDER BY timestamp
+#     """
+#     clickhouse_client.command(create_table_query)
+
+#     data_to_insert = [
+#         (
+#             row["location"],
+#             safe_float(row.get("lat", 0.0)),
+#             safe_float(row.get("lon", 0.0)),
+#             row.get("temp", 0.0), 
+#             row.get("feels_like", 0.0),
+#             row.get("temp_min", 0.0),
+#             row.get("temp_max", 0.0),
+#             row.get("pressure", 0), 
+#             row.get("humidity", 0),
+#             row.get("wind_speed", 0.0),
+#             row.get("wind_deg", 0),
+#             row.get("wind_gust", 0.0),
+#             row.get("clouds", 0),
+#             row.get("timestamp", 0),
+#             row.get("dt", 0),
+#             format_unix_to_gmt7_string(row.get("dt", 0)),
+#         )
+#         for row in data
+#     ]
+
+#     utc_now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S GMT+0")
+#     logging.info(f"Uploading {len(data)} records to ClickHouse at {utc_now}")
+
+#     clickhouse_client.insert(table_name, data_to_insert)
+
+#     await clear_redis()  
+#     logging.info(f"Uploaded {len(data)} records to ClickHouse and cleared Redis.")
+
+#     clickhouse_client.close()
