fc1c5a4df3cc6f27de8cd00d92e559dd7b6b820b
ernestbenedictus <m010d4ky1623@bangkit.academy>
2025-06-01 20:47:22 +0700

    add batch upload testing #9

--- writer/clickhouse_writer.py
+++ writer/clickhouse_writer.py
@@ -3,7 +3,7 @@ import clickhouse_connect
 import httpx
 from datetime import datetime, timezone, timedelta
 from config.utils import get_env_value
-from datastore.redis_store import group_keys_by_location, get_weather_data_by_keys, clear_redis
+from datastore.redis_store import get_all_weather_data, group_keys_by_location, get_weather_data_by_keys, clear_redis
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
@@ -76,64 +76,64 @@ async def bulk_write_to_clickhouse():
         return
 
     previous_hour = datetime.now(timezone.utc) - timedelta(hours=1)
-    timestamp_str = previous_hour.strftime("%Y%m%d")
+    timestamp_str = previous_hour.strftime("%Y%m%d_%H%M%S")
 
     total_inserted = 0
 
-    for location, keys in grouped_keys.items():
-        weather_data = await get_weather_data_by_keys(keys)
-        if not weather_data:
-            continue
+    # for location, keys in grouped_keys.items():
+    weather_data = await get_all_weather_data()
+    # if not weather_data:
+    #     continue
 
-        table_name = f"weather_{location}_{timestamp_str}"
+    table_name = f"weather_{timestamp_str}"
 
-        create_table_query = f"""
-        CREATE TABLE IF NOT EXISTS {table_name} (
-            dt Int32,
-            timestamp String,
-            lat Float64,
-            lon Float64,
-            location String,
-            description String,
-            temp Float32,
-            feels_like Float32,
-            pressure Int32,
-            humidity Int32,
-            wind_speed Float32,
-            wind_deg Int32,
-            wind_gust Float32,
-            clouds Int32,
-        ) ENGINE = MergeTree()
-        ORDER BY timestamp
-        """
-        clickhouse_client.command(create_table_query)
+    create_table_query = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        dt Int32,
+        timestamp String,
+        lat Float64,
+        lon Float64,
+        location String,
+        description String,
+        temp Float32,
+        feels_like Float32,
+        pressure Int32,
+        humidity Int32,
+        wind_speed Float32,
+        wind_deg Int32,
+        wind_gust Float32,
+        clouds Int32,
+    ) ENGINE = MergeTree()
+    ORDER BY timestamp
+    """
+    clickhouse_client.command(create_table_query)
 
-        await register_table_api(table_name, location, timestamp_str)
+    # await register_table_api(table_name, location, timestamp_str)
 
-        data_to_insert = [
-            (
-                row.get("dt", 0),
-                format_unix_to_utc_string(row.get("dt", 0)),
-                safe_float(row.get("lat", 0.0)),
-                safe_float(row.get("lon", 0.0)),
-                row["location"],
-                row["description"],
-                row.get("temp", 0.0), 
-                row.get("feels_like", 0.0),
-                row.get("pressure", 0), 
-                row.get("humidity", 0),
-                row.get("wind_speed", 0.0),
-                row.get("wind_deg", 0),
-                row.get("wind_gust", 0.0),
-                row.get("clouds", 0),
-            )
-            for row in weather_data
-        ]
+    data_to_insert = [
+        (
+            row.get("dt", 0),
+            format_unix_to_utc_string(row.get("dt", 0)),
+            safe_float(row.get("lat", 0.0)),
+            safe_float(row.get("lon", 0.0)),
+            row["location"],
+            row["description"],
+            row.get("temp", 0.0), 
+            row.get("feels_like", 0.0),
+            row.get("pressure", 0), 
+            row.get("humidity", 0),
+            row.get("wind_speed", 0.0),
+            row.get("wind_deg", 0),
+            row.get("wind_gust", 0.0),
+            row.get("clouds", 0),
+        )
+        for row in weather_data # type: ignore
+    ]
 
-        clickhouse_client.insert(table_name, data_to_insert)
-        logging.info(f"Uploaded {len(data_to_insert)} records to table {table_name}.")
+    clickhouse_client.insert(table_name, data_to_insert)
+    logging.info(f"Uploaded {len(data_to_insert)} records to table {table_name}.")
 
-        total_inserted += len(data_to_insert)
+    total_inserted += len(data_to_insert)
 
     if total_inserted > 0:
         await clear_redis()
